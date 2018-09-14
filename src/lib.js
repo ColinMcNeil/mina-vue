0ac67eb1633b4bb7b5a89e0728b8a8fca66b8fbe
colinmcneil <colinmcneil@gmail.com>
2018-09-14 10:10:19 -0400

    build package

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,27 @@
-import vueMin from './components/VMinCover-Stencil'
+import VMinCoverStencil from './components/VMinCover-Stencil'
+import VMinCoverRepel from './components/VMinCover-Stencil'
+import VMinCoverPeek from './components/VMinCover-Stencil'
+import VMinTile from './components/VMin-Tile'
+import VMinTileGrid from './components/VMin-Tile-Grid'
+import VMinNav from './components/VMin-Nav'
+import VMinDash from './components/VMin-Dash'
+import VMinColorPick from './components/VMin-ColorPick'
+import VMinButton from './components/VMin-Button'
+import VMinButtonGroup from './components/VMin-Button-Group'
+import VMinBreadcrumb from './components/VMin-Breadcrumb'
+import VMinAlerts from './components/VMin-Alerts'
 
-export default vueMin
+export default {
+  VMinCoverStencil,
+  VMinCoverRepel,
+  VMinCoverPeek,
+  VMinTile,
+  VMinTileGrid,
+  VMinNav,
+  VMinDash,
+  VMinColorPick,
+  VMinButton,
+  VMinButtonGroup,
+  VMinBreadcrumb,
+  VMinAlerts
+}
